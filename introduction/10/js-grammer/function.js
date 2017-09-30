f8c38d8c010964791d586cbb4ca1040a25b7bc87
n4cl <devn4cl@gmail.com>
2017-09-30 19:44:33 +0900

    Add lecture 10

---
+++ introduction/10/js-grammer/function.js
@@ -0,0 +1,14 @@
+(function(){
+    'use strict';
+    var admission = new Date(2017, 9, 11, 0, 0);
+
+    function updateParagraph(){
+        var now = new Date();
+        var days = (now.getTime() - admission.getTime()) / (1000 * 60 * 60 * 24);
+        days = (now.getTime() - admission.getTime())
+        // 日数だけ表示するように
+        days = Math.floor(days);
+        document.getElementById('admission-time').innerText = 'N予備校に入学してから' + days + '日経過。';
+    }
+    updateParagraph();
+})();
