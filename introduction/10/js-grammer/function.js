da452521a4638aa83738c4573ecfc86ffd4e509e
n4cl <devn4cl@gmail.com>
2017-10-01 03:38:53 +0900

    Fix admission day
    Dateオブジェクトは0起点

--- introduction/10/js-grammer/function.js
+++ introduction/10/js-grammer/function.js
@@ -1,11 +1,10 @@
 (function(){
     'use strict';
-    var admission = new Date(2017, 9, 11, 0, 0);
+    var admission = new Date(2017, 8, 11, 0, 0);
 
     function updateParagraph(){
         var now = new Date();
-        var days = (now.getTime() - admission.getTime()) / (1000 * 60 * 60 * 24);
-        days = (now.getTime() - admission.getTime())
+        var days = (now.getTime() - admission.getTime()) / 1000 / 60 / 60 / 24;
         // 日数だけ表示するように
         days = Math.floor(days);
         document.getElementById('admission-time').innerText = 'N予備校に入学してから' + days + '日経過。';
